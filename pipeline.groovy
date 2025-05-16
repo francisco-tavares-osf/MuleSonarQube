31c3156861be684b0e1c793a352f49b40eb23831
Francisco <francisco.tavares@osf.digital>
2025-05-16 17:20:42 +0100

    testing

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,17 +1,94 @@
-def getProjectName() {
-    if (fileExists('mule-artifact.json')) {
-        def artifactJson = readJSON file: 'mule-artifact.json'
-        return artifactJson.name ?: "unknown-mule-project"
-    } else if (fileExists('pom.xml')) {
-        def pomContent = readFile 'pom.xml'
-        def match = pomContent =~ /<artifactId>(.*?)<\/artifactId>/
-        if (match) {
-            return match[0][1]
+pipeline {
+    agent any
+    
+    environment {
+        MAIN_BRANCH = 'main'
+        REPOSITORY_URL = 'https://github.com/francisco-tavares-osf/MuleSonarQube.git'
+        GITHUB_TOKEN = credentials('github-token')
+        SONAR_TOKEN = credentials('sonar-token')
+        SONAR_HOST = 'http://sonarqubemule:9000' 
+        SONAR_BASE_KEY = 'mule-projects'
+        
+        MULE_ENDPOINT = 'http://localhost:8081/caminho'
+        MULESOFT_TIMEOUT = 1
+        
+        SONAR_METRICS = 'bugs,vulnerabilities,code_smells,coverage,duplicated_lines_density,reliability_rating,security_rating'
+        SONAR_TIMEOUT_MINUTES=1
+    }
+        
+    stages {
+        stage('Clone Repository') {
+            steps {
+                script {
+                    cleanWs()
+                    echo "Cloning repository ${REPOSITORY_URL}"
+                    withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
+                        sh "git clone https://${GITHUB_TOKEN}@github.com/francisco-tavares-osf/MuleSonarQube.git repo"
+                    }
+                    
+                    dir('repo') {
+                        //lista todas as branches
+                        sh "git branch -a"
+                        sh "git checkout ${MAIN_BRANCH}"
+                    }
+                }
+            }
+        }
+        
+        stage('Fetch PRs') {
+            steps {
+                script {
+                    dir('repo') {
+                        echo "Getting PRs to main branch"
+
+                        withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
+                            def response = sh (
+                                script: """curl -X GET -s -H "Authorization: Bearer ${GITHUB_TOKEN}" \\
+                                           -H "Accept: application/vnd.github+json" \\
+                                           https://api.github.com/repos/francisco-tavares-osf/MuleSonarQube/pulls""",
+                                returnStdout: true
+                            ).trim()
+                   
+                            echo "API Response: ${response}"
+                            
+                            def prs = readJSON text: response
+
+                            if (prs && prs.size() > 0) {
+                                echo "Processing PRs..."
+                                
+                                prs.each { pr -> 
+                                
+                                //Secure Hash Algorithm (sha) é um identifier, e essencial para criar branches e accuracy
+                                    def prHash = pr.head.sha
+                                    def prNum = pr.number
+                                    def branchName = "pr-${prNum}-analysis"
+                                    def projectName = "Mule-Sonar-${prNum}"
+                                    def projectKey = generateProjectKey(projectName, prNum)
+                                    
+                                    echo "Processing PR #${prNum} (${prHash})"
+
+                                    //Cria um branch para cada PR
+                                    sh "git checkout -b ${branchName} ${prHash}" 
+                                    
+                                    
+                                    analyzeMuleProject(prNum, prHash, projectName, projectKey)
+
+                                    //Volta para branch principal
+                                    sh "git checkout ${MAIN_BRANCH}"
+                                    //Elimina o branch criado do PR
+                                    sh "git branch -D ${branchName}"
+                                }
+                            } else {
+                                echo "Nenhum PR encontrado para análise"
+                            }
+                        }
+                    }
+                        
+                }
+            }
         }
     }
-    return "unknown-project"
 }
-
 def generateProjectKey(projectName, prNum) {
     def sanitizedName = projectName.toLowerCase().replaceAll(/[^a-z0-9-_]/, '-')
     return "${SONAR_BASE_KEY}:${sanitizedName}:pr-${prNum}"
@@ -19,24 +96,39 @@ def generateProjectKey(projectName, prNum) {
 
 def analyzeMuleProject(prNum, prHash, projectName, projectKey) {
     stage("PR #${prNum} analysis") {
+        sh 'pwd'
+        sh 'python3 /var/jenkins_home/my_script.py'
+        def jsonFilePath = '/var/jenkins_home/workspace/SonarPipeline/repo/sonar-structure-rules-issues.json'
+        sh 'c/var/jenkins_home/workspace/SonarPipeline/repo/sonar-structure-rules-issues.json'
         withSonarQubeEnv('SonarQube') {
             sh """
+                pwd 
+
                 sonar-scanner \\
                 -Dsonar.projectKey=${projectKey} \\
                 -Dsonar.projectName=${projectName} \\
                 -Dsonar.projectVersion='PR-${prNum}' \\
                 -Dsonar.sources=. \\
+                -Dsonar.externalIssuesReportPaths=${jsonFilePath} \\
                 -Dsonar.host.url=${SONAR_HOST} \\
                 -Dsonar.login=${SONAR_TOKEN} \\
                 -Dsonar.sourceEncoding=UTF-8 \\
                 -Dsonar.exclusions=target/**,**/*.jar \\
                 -Dsonar.modules.recalculateRoot=true \\
                 -Dsonar.mule.app=true \\
-                -Dsonar.language=mule
+                -Dsonar.language=mule \\
+                -Dsonar.verbose=true \\
+                -X
             """
         }
-        timeout(time: SONAR_TIMEOUT_MINUTES, unit: 'MINUTES') {
-            waitForQualityGate abortPipeline: false
+        
+        stage('Quality Gate'){
+            timeout(time: SONAR_TIMEOUT_MINUTES, unit: 'MINUTES') {
+                def qualityGate = waitForQualityGate()
+                if (qualityGate.status != 'OK') {
+                    error "Pipeline aborted due to quality gate failure: ${qualityGate.status}"
+                }
+            }
         }
         
         def sonarResults = sh(script: """
@@ -53,7 +145,7 @@ def analyzeMuleProject(prNum, prHash, projectName, projectKey) {
            "sonar_results": ${sonarResults}
         }
         """
-    
+    /*
         timeout(time: MULESOFT_TIMEOUT, unit: 'SECONDS') {
             sh """
                 curl -X POST \\
@@ -62,90 +154,7 @@ def analyzeMuleProject(prNum, prHash, projectName, projectKey) {
                 ${MULE_ENDPOINT}
             """
         }
+        */
         echo "Project ${projectName} with PR number #${prNum} done and results sent."
     }
-}
-
-pipeline {
-    agent any
-    
-    environment {
-        MAIN_BRANCH = 'main'
-        REPOSITORY_URL = 'https://github.com/francisco-tavares-osf/MuleSonarQube.git'
-        GITHUB_TOKEN = credentials('github-token')
-        SONAR_TOKEN = credentials('sonarqube-token')
-
-        SONAR_HOST = 'http://localhost:9000' 
-        
-        MULE_ENDPOINT = 'http://localhost:8081/caminho'
-        
-        SONAR_METRICS = 'bugs,vulnerabilities,code_smells,coverage,duplicated_lines_density,reliability_rating,security_rating'
-    }
-        
-    stages {
-        stage('Clone Repository') {
-            steps {
-                script {
-                    cleanWs()
-                    echo "Cloning repository ${REPOSITORY_URL}"
-                    withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
-                        sh "git clone https://${GITHUB_TOKEN}@github.com/francisco-tavares-osf/MuleSonarQube.git repo"
-                    }
-                    
-                    dir('repo') {
-                        sh "git branch -a"
-                        sh "git checkout ${MAIN_BRANCH}"
-                    }
-                }
-            }
-        }
-        
-        stage('Fetch PRs') {
-            steps {
-                script {
-                    dir('repo') {
-                        echo "Getting PRs to main branch"
-
-                        withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
-                            def response = sh (
-                                script: """curl -s -H "Authorization: Bearer ${GITHUB_TOKEN}" \\
-                                           -H "Accept: application/vnd.github+json" \\
-                                           https://api.github.com/repos/francisco-tavares-osf/MuleSonarQube/pulls""",
-                                returnStdout: true
-                            ).trim()
-                            
-                            echo "API Response: ${response}"
-                            
-                            def prs = readJSON text: response
-
-                            if (prs && prs.size() > 0) {
-                                echo "Processing PRs..."
-                                
-                                prs.each { pr -> 
-                                    def prHash = pr.head.sha
-                                    def prNum = pr.number
-                                    def branchName = "pr-${prNum}-analysis"
-                                    def projectName = getProjectName()
-                                    def projectKey = generateProjectKey(projectName, prNum)
-                                    
-                                    echo "Processing PR #${prNum} (${prHash})"
-                                    
-                                    sh "git checkout -b ${branchName} ${prHash}"
-
-                                    analyzeMuleProject(prNum, prHash, projectName, projectKey)
-                                    
-                                    sh "git checkout ${MAIN_BRANCH}"
-                                    sh "git branch -D ${branchName}"
-                                }
-                            } else {
-                                echo "Nenhum PR encontrado para análise"
-                            }
-                        }
-                    }
-                        
-                }
-            }
-        }
-    }
-}
-            
+}           
